4b6afd8ece77d4a54442acf7a66b49eb36f3ca17
XMs <xms@nm.ru>
2012-08-11 17:04:14 +0400

    Update for README, remake structure, add compile and commit scripts

---
+++ compile.sh
@@ -0,0 +1,4 @@
+pdflatex study-plan.tex > /dev/null
+bibtex study-plan.aux > /dev/null
+pdflatex study-plan.tex > /dev/null
+pdflatex study-plan.tex > /dev/null
---
+++ do-commit.sh
@@ -0,0 +1,4 @@
+rm -f *.{aux,bbl,blg,log,pdf,toc} */*.aux */*/*.pdf
+git add .
+git commit
+git push
