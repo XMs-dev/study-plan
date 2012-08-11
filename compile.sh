c9b951b0521e3584ef36e62021bac2fbf7968cfc
XMs <xms@nm.ru>
2012-08-12 01:00:37 +0400

    Little fix to compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 rm -f *.{aux,bll,blg,log,pdf,toc,bak,out} */*.{pdf,aux,bak} */*/*.{pdf,aux,bak}
-pdflatex study-plan.tex > /dev/null
+pdflatex study-plan.tex
 bibtex study-plan.aux > /dev/null
 pdflatex study-plan.tex > /dev/null
 pdflatex study-plan.tex > /dev/null
