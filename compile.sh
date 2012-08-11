5a4bc8b0bec440e330d04587e91410629ebdd7ec
XMs <xms@nm.ru>
2012-08-12 00:32:09 +0400

    Addition to sysutils info, update for scripts, and maybe something else

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+rm -f *.{aux,bll,blg,log,pdf,toc,bak,out} */*.{pdf,aux,bak} */*/*.{pdf,aux,bak}
 pdflatex study-plan.tex > /dev/null
 bibtex study-plan.aux > /dev/null
 pdflatex study-plan.tex > /dev/null
--- do-commit.sh
+++ do-commit.sh
@@ -1,4 +1,4 @@
-rm -f *.{aux,bbl,blg,log,pdf,toc,bak} */*.{aux,bak} */*/*.{pdf,bak}
+rm -f *.{aux,bbl,blg,log,pdf,toc,bak,out} */*.{pdf,aux,bak} */*/*.{pdf,aux,bak}
 git add .
 git commit
 git push
