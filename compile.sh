1d1c2a792315e5dab671ff1017b469bd4cdd9194
XMs <xms@nm.ru>
2012-08-16 19:28:59 +0400

    Add text about simple proccess control to base/OS/Structure and modify compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-rm -f *.{aux,bll,blg,log,pdf,toc,bak,out} */*.{pdf,aux,bak} */*/*.{pdf,aux,bak}
+rm -f *.{aux,bll,blg,log,pdf,toc,bak,out,dvi,ps} */*.{pdf,aux,bak} */*/*.{pdf,aux,bak}
 pdflatex study-plan.tex
 bibtex study-plan.aux > /dev/null
 pdflatex study-plan.tex > /dev/null
